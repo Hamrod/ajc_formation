69b5022680a2e061675254b8a3f1bdb0e9e5964f
Malo <moy_97@hotmail.fr>
2022-12-19 17:18:46 +0100

    Modif de User

--- src/main/java/ajc/formation/alten/model/User.java
+++ src/main/java/ajc/formation/alten/model/User.java
@@ -3,26 +3,47 @@ package ajc.formation.alten.model;
 public class User {
 	private int id;
 	private String name;
-	public User(int id, String name) {
+	private int age;
+
+	public User(int id, String name, int age) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.age = age;
 	}
+
 	public User() {
 		super();
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
+	/**
+	 * @return int return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
